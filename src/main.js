1005d077a7ae8f5f33d0b3a8b5e8692141958712
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-22 12:05:54 +0000

    Update images and the main structure

---
+++ src/main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import stars from './images/stars.jpg'
+import './App.css';
+
+export default function Main () {
+    return(
+        <div>
+            <section>
+                <div className='background'>
+                    <div className='bg' data-module="ImageFit">
+                        <img className='bgimg' src={stars} alt='mexity' data-screenratio="1.3" data-aspectratio="1x2, 4000x4000"/>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
