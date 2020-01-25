16e9b53262c0681bee78dd1b320fb49cbfa41577
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-25 17:01:43 +0000

    trying new thme

---
+++ src/timesThem/timesThem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Times () {
+    retunr{
+        <div className='timesbg'>
+            <div className='timesHeader'>
+                <h1></h1>
+                <label></label>
+            </div>
+            <div className='timesBody'>
+                <div className='timesProfile'></div>
+                <div className='timesProject'></div>
+                <div className='timesSkills'></div>
+            </div>
+        </div>
+    }
+}
