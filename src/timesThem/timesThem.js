1010579678902ca85f984cde8b9da47d32ef4a9b
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-25 17:05:43 +0000

    new them layout

--- src/timesThem/timesThem.js
+++ src/timesThem/timesThem.js
@@ -4,11 +4,14 @@ export default function Times () {
     retunr{
         <div className='timesbg'>
             <div className='timesHeader'>
-                <h1></h1>
+                <h1 className='kainy'></h1>
                 <label></label>
             </div>
             <div className='timesBody'>
-                <div className='timesProfile'></div>
+                <div className='timesProfile'>
+                    <img src={} alt=''/>
+                    <p><label className='theFirst'>L</label>orem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nulla velit dolorem iusto quisquam sequi iste tempora. Deleniti labore placeat illo eos molestiae velit, beatae cum, nostrum maiores rerum repellat neque, asperiores exercitationem magnam officia quam consequuntur doloremque necessitatibus ut quos excepturi iure odit laborum minima. Velit error reiciendis ut possimus laborum sit porro dolor dolorem cumque praesentium nesciunt adipisci provident ex reprehenderit a omnis iste non hic deleniti totam eveniet consectetur, natus similique? Reprehenderit mollitia voluptas unde adipisci distinctio repellat officiis, praesentium, placeat, incidunt saepe dolorem repellendus illo magni possimus assumenda. Quos cupiditate suscipit a, dolores </p>
+                </div>
                 <div className='timesProject'></div>
                 <div className='timesSkills'></div>
             </div>
