3666ad523d2da48033002ba088966dc9e1fdb63b
KainyRyu <KainyRyu@users.noreply.github.com>
2020-01-25 17:07:05 +0000

    big layouts

--- src/timesThem/timesThem.js
+++ src/timesThem/timesThem.js
@@ -12,8 +12,14 @@ export default function Times () {
                     <img src={} alt=''/>
                     <p><label className='theFirst'>L</label>orem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nulla velit dolorem iusto quisquam sequi iste tempora. Deleniti labore placeat illo eos molestiae velit, beatae cum, nostrum maiores rerum repellat neque, asperiores exercitationem magnam officia quam consequuntur doloremque necessitatibus ut quos excepturi iure odit laborum minima. Velit error reiciendis ut possimus laborum sit porro dolor dolorem cumque praesentium nesciunt adipisci provident ex reprehenderit a omnis iste non hic deleniti totam eveniet consectetur, natus similique? Reprehenderit mollitia voluptas unde adipisci distinctio repellat officiis, praesentium, placeat, incidunt saepe dolorem repellendus illo magni possimus assumenda. Quos cupiditate suscipit a, dolores </p>
                 </div>
-                <div className='timesProject'></div>
-                <div className='timesSkills'></div>
+                <div className='timesProject'>
+                    <div>
+                        <p></p>
+                    </div>
+                </div>
+                <div className='timesSkills'>
+                    <h1></h1>
+                </div>
             </div>
         </div>
     }
