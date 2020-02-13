ab866a3745ee41623ba4984733afdf53ce1ec1a6
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-13 18:32:23 +0000

    reponsive layout for the profile

--- src/profile.js
+++ src/profile.js
@@ -6,7 +6,7 @@ export default function Profile() {
     return(
         <div className='timesProfile flexColumn'>
             <img className="image" src={profileImg} alt='Me'/>
-            <div className='summary topBorder'>
+            <div className='summary'>
                 <p><label className='theFirst'>L</label>orem ipsum, dolor sit amet consectetur adipisicing elit. Soluta, nulla velit dolorem iusto quisquam sequi iste tempora. Deleniti labore placeat illo eos molestiae velit, beatae cum, nostrum maiores rerum repellat neque, asperiores exercitationem magnam officia quam consequuntur doloremque necessitatibus ut quos excepturi iure odit laborum minima. dolores </p>
                 <p>Velit error reiciendis ut possimus laborum sit porro dolor dolorem cumque praesentium nesciunt adipisci provident ex reprehenderit a omnis iste non hic deleniti totam eveniet consectetur, natus similique? Reprehenderit mollitia voluptas unde adipisci distinctio repellat officiis, praesentium, placeat, incidunt saepe dolorem repellendus illo magni possimus assumenda. Quos cupiditate suscipit a, dolores </p>
             </div>
