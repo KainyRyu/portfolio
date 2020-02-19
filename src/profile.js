1baab90f335f2cdacf4ef8d1dba3591cb5a51a07
KainyRyu <KainyRyu@users.noreply.github.com>
2020-02-19 12:00:31 +0000

    changed some layout for mobile and web

--- src/profile.js
+++ src/profile.js
@@ -6,9 +6,8 @@ export default function Profile() {
   return (
     <div className="timesProfile flexColumn">
       <img className="image" src={profileImg} alt="Me" />
-      <div className="project-summary">
-        <p>
-          <label className="theFirst">A</label> full-stack developer keen to
+      <div className="profile-summary">
+          <label className="the-first">A</label> full-stack developer keen to
           learn new skills and collaborate with others. A firm believer in
           discipline and self-driven process in learning. working well in
           specialist teams in exciting and continually developing environments.
@@ -16,7 +15,7 @@ export default function Profile() {
           sociable and tactful personality, always enjoying the challenge of
           working under pressure either independently or as a member of a team
           within an agile team.
-        </p>
+
         {/* <p>Started coding for fun and now I am more serious </p> */}
       </div>
     </div>
