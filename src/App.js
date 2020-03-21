26a9c4ae0d33ad333050df1d8296a7a8d1eacb2e
KainyRyu <KainyRyu@users.noreply.github.com>
2020-03-21 22:47:49 +0000

    make it better structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
-import Times from "./components/timesThem";
-import "./App.css";
+import React from 'react';
+import './timesThem.css';
+import Profile from './profile'
+import Header from './Header';
+import Activities from './activities/Activities';
+import Project from './project/Projects';
 
 function App() {
   return (
     <div className="App">
-      <Times />
+      <div className='timesbg'>
+            <Header />
+            <div className='timesBody'>
+                <Profile />
+                <div className="body-right">
+                    <h1 className='skills' >
+                        “Skills :  JavaScript, ReactJS, NodeJS, mongoDB, Express, HTML5, CSS3, Bootstrap3 and more are coming..” 
+                    </h1>
+                    <Activities />
+                </div>
+            </div>
+            <div className="project-label-container">
+                <h1 className='project-label'>PROJECTS;</h1> 
+                <span className='project-label-span'>click the 
+                    <span className="underline"> images</span> or the 
+                    <span className="underline"> underlined text</span> to see my work.
+                </span>
+            </div>
+            <Project />
+        </div>
     </div>
   );
 }
--- src/components/timesThem.js
+++ src/components/timesThem.js
@@ -2,32 +2,13 @@ import React from 'react';
 import './timesThem.css';
 import Profile from './profile'
 import Header from './Header';
-import Activities from './Activities';
+import Activities from './activities/Activities';
 import Project from './project/Projects';
 
 
 export default function Times () {
     return(
-        <div className='timesbg'>
-            <Header />
-            <div className='timesBody'>
-                <Profile />
-                <div className="body-right">
-                    <h1 className='skills' >
-                        “Skills :  JavaScript, ReactJS, NodeJS, mongoDB, Express, HTML5, CSS3, Bootstrap3 and more are coming..” 
-                    </h1>
-                    <Activities />
-                </div>
-            </div>
-            <div className="project-label-container">
-                <h1 className='project-label'>PROJECTS;</h1> 
-                <span className='project-label-span'>click the 
-                    <span className="underline"> images</span> or the 
-                    <span className="underline"> underlined text</span> to see my work.
-                </span>
-            </div>
-            <Project />
-        </div>
+        
     )
 }
 
