6a0f74388a36b9312cc3bc70730c3249f1fabb7c
KainyRyu <KainyRyu@users.noreply.github.com>
2020-04-22 01:06:35 +0100

    some changes

--- src/components/activities/Activities.js
+++ src/components/activities/Activities.js
@@ -7,7 +7,19 @@ export default function Activities () {
             <h1>Activities</h1>
             <div className="activities-subject activity_first">
                 <h3> Hackathon </h3>
-                    Participated in 'UK vs COVID-19' Hackathon during the quarantine period; more about the project<a href="#project-fourth"> tell my GP</a>, and you'll see more on the<a href="https://medium.com/ukvscovidhack/tell-my-gp-63de96231e38" target="_blank"> Medium post</a>
+                    Participated in 'UK vs COVID-19' Hackathon during the quarantine period; more about the project
+                    <a 
+                        href="#project-fourth"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    > tell my GP</a>
+                    , and you'll see more on the
+                    <a 
+                        href="https://medium.com/ukvscovidhack/tell-my-gp-63de96231e38" 
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    > Medium post
+                    </a>
             </div>
             <div className="activities-subject activity_first">
                 <h3> Coaching </h3>
@@ -21,7 +33,14 @@ export default function Activities () {
             </div>
             <div className="activities-subject activity_third">
                 <h3> Running Workshops </h3>
-                Kodiri : Running a <a href="https://youtu.be/GAkdWTmPw9Q" target="_blank"> live streaming workshops</a> about react advanced skills.
+                Kodiri : Running a 
+                <a 
+                    href="https://youtu.be/GAkdWTmPw9Q" 
+                    rel="noopener noreferrer"
+                    target="_blank"
+                > live streaming workshops 
+                </a> 
+                about react advanced skills.
                 </div>
         </div>
     )
