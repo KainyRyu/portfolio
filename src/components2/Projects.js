4df2f64e4794f1aa79ed1ff731af8613a264d1db
kainy ryu <kainy2109@gmail.com>
2022-03-29 21:27:09 +0900

    Update img location

--- src/components2/Projects.js
+++ src/components2/Projects.js
@@ -2,48 +2,146 @@ import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { PROJECTS } from '../lib/contents';
 
-const projectList = document.querySelectorAll('.project');
+import JoaraMobile from '../assets/projects/mw_mobile.png';
+import PreJoara from '../assets/projects/pre_joara.png';
+import DesktopJoara from '../assets/projects/mw_renew.png';
+import JoaraWTOld from '../assets/projects/webtoon_old.png';
+import JoaraWTRenew from '../assets/projects/webtoon_new.png';
+
 export default function Project2() {
+  const mainTitleRef = useRef(null);
+  const [position, setPosition] = useState(0);
+  const [pageYOffset, setPageYOffset] = useState(0);
   const [refList, setRefList] = useState(0);
   const [ioIdx, setIoIdx] = useState(0);
   const listWrapRef = useRef(null);
   const listElems = listWrapRef.current && listWrapRef?.current.children;
 
-  const io = new IntersectionObserver((entries, observer) => {
-    setIoIdx(entries[0].target.dataset.index);
+  const action = {
+    moveSubject(elem) {
+      if (elem) {
+        // Move Subject
+        elem.style.transform = `translateX(${window.innerWidth}px)`;
+      }
+    },
+    goBack(elem) {
+      if (elem) {
+        // Move back Subject
+        elem.style.transform = `translateX(-${window.innerWidth}px)`;
+      }
+    },
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    const item = entries[0];
+    console.log(item.target.dataset.projectindex, item.boundingClientRect.top);
+    console.log(entries);
+    // entries.forEach((entry) => {
+    //   window.addEventListener('scroll', () => {
+    //     // console.log(entry.target.dataset.projectindex, entry);
+    //     console.log(entry.target.dataset.projectindex, entry.boundingClientRect.top);
+
+    //     // console.log(
+    //     //   window.innerHeight * 0.9,
+    //     //   entry.boundingClientRect.top, // boudingClientRect.top <- this value is absolute
+    //     //   window.innerHeight * 0.2,
+    //     //   entry.boundingClientRect.top,
+    //     //   entry.target.dataset.projectindex,
+    //     // );
+    //     if (entry.isIntersecting) {
+    //       // console.log(entry.isIntersecting, entry.target.dataset.projectindex);
+    //       // console.log(entry.target.dataset.projectindex, entry.boundingClientRect.top);
+    //       // console.log('children --- ', entry.target.children[0].children);
+    //     }
+    //   });
+    // });
   });
-  console.log(ioIdx);
-
+  const onScroll = () => {
+    setPosition(window.scrollY);
+    setPageYOffset(document.body.offsetHeight);
+  };
+  // console.log({ scrollY: position, pageYOffset });
+  // console.log({ outerHeight: window.outerHeight, innerHeight: window.innerHeight });
   useEffect(() => {
-    listWrapRef.current && setRefList([...listWrapRef?.current.children]);
-  }, [listElems]);
-
-  useEffect(() => {
-    console.log('refList--', refList);
-    refList &&
-      refList.forEach((element) => {
-        io.observe(element);
-      });
-  }, [refList]);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+  // console.log({ position, pageYOffset });
 
   useEffect(() => {
     refList &&
-      refList.map((item) => {
+      refList.map((item, idx) => {
+        window.addEventListener('scroll', () => {
+          // console.log({ ioIdx, boundingRect: boundingRect.top });
+          observer.observe(refList[idx]);
+        });
         const boundingRect = item.getBoundingClientRect();
         if (boundingRect.top > window.innerHeight * 0.1) {
           window.addEventListener('scroll', () => {
-            console.log({ ioIdx, boundingRect: boundingRect.top });
+            // console.log({ ioIdx, boundingRect: boundingRect.top });
           });
         }
+
+        return () => window.removeEventListener('scroll');
       });
   }, [refList, ioIdx]);
 
   return (
     <ProjectSection>
+      <ScrollImgs>
+        <ImgWrap>
+          <img
+            className="scroll-img"
+            src={JoaraWTOld}
+            alt="old webtoon"
+            style={{ height: '600px' }}
+          />
+        </ImgWrap>
+        <ImgWrap>
+          <img
+            className="scroll-img"
+            src={JoaraWTRenew}
+            alt="new webtoon"
+            style={{
+              height: '600px',
+            }}
+          />
+        </ImgWrap>
+        <ImgWrap>
+          <img
+            className="scroll-img"
+            src={PreJoara}
+            alt="main mobile"
+            style={{
+              height: '600px',
+            }}
+          />
+        </ImgWrap>
+        <ImgWrap>
+          <img
+            className="scroll-img"
+            src={JoaraMobile}
+            alt="renewed mobile"
+            style={{
+              height: '600px',
+            }}
+          />
+        </ImgWrap>
+        <ImgWrap>
+          <img
+            className="scroll-img"
+            src={DesktopJoara}
+            alt="renewed mobile"
+            style={{
+              height: '600px',
+            }}
+          />
+        </ImgWrap>
+      </ScrollImgs>
       <ProejctListWrap ref={listWrapRef}>
         {PROJECTS.map(({ subject, period, summary, details }, idx) => {
           return (
-            <ProjectWrap key={idx} className="project" data-index={idx}>
+            <ProjectWrap key={idx} className="project" data-projectindex={idx}>
               <Project idx={idx}>
                 <div>Img</div>
                 <h1>subject: {subject}</h1>
@@ -63,39 +161,43 @@ export default function Project2() {
   );
 }
 
-const ProjectSection = styled.div`
-  // text-align: center;
-  margin-top: 220px;
-  overflow: hidden;
+const ProjectSection = styled.section`
+  margin-top: 50px;
   box-sizing: border-box;
+  border: red 1px solid;
   padding: 5px;
+  overflow: hidden;
   @media (min-width: 600px) {
   }
 `;
 
 const ProejctListWrap = styled.div`
-  border: red 1px solid;
   top: 210px;
 `;
 
 const ProjectWrap = styled.div`
-  position: relative;
   width: 100%;
-  padding-bottom: 70vh;
-  border-bottom: ${(props) => props['data-index']}px solid black;
+  height: 80vh;
+  border-bottom: ${(props) => props['data-projectindex']}px solid black;
 `;
 
 const Project = styled.div`
   ${(props) => (props.idx % 2 === 0 ? 'left' : 'right')}: -10%;
-  position: absolute;
   white-space: nowrap;
   border: blue 1px dotted;
-
-  transition: 0.5s;
   transform: translateX(${(props) => (props.idx % 2 === 0 ? '25' : '-25')}%);
   @media (min-width: 1000px) {
     // max-width: 500%;
   }
 `;
 
-const DetailList = styled.li``;
+const ScrollImgs = styled.div`
+  position: sticky;
+  top: 200px;
+  height: 80vh;
+`;
+
+const ImgWrap = styled.div`
+  position: absolute;
+  // opacity: 0;
+`;
